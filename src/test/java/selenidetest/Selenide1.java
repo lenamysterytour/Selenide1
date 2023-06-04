191b03c49550e5b15c28c43de4c2b8460028e066
Bolotnikova Elena <lenamysterytour@gmail.com>
2023-06-04 12:22:14 +0300

    Selenide 1 homework

---
+++ src/test/java/selenidetest/Selenide1.java
@@ -0,0 +1,42 @@
+package selenidetest;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.*;
+
+public class Selenide1 {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void shouldFindSelenideRepositoryAtTheTop() {
+        //открыть главную страницу
+        open("https://github.com");
+        //ввести в поиск селенид и нажать энтер
+        ($(".header-search-input")).setValue("selenide").pressEnter();
+
+        //кликнуть на первый репозиторий из списка найденных
+        $$("ul.repo-list li").first().$("a").click();
+
+        $("nav.js-repo-nav").$("ul li").sibling(4).click();
+
+        $("[placeholder='Find a page…']").setValue("SoftAssertions");
+
+        $(byText("SoftAssertions")).click();
+
+//проверка: кусок кода о JUnit 5 содержит метод тест
+        $("#user-content-3-using-junit5-extend-test-class").parent().sibling(0).shouldHave(text("@Test"));
+
+
+    }
+}
