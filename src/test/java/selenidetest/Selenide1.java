38bfefecc6db7e11ef936c255035a54bb04f8e6e
Bolotnikova Elena <lenamysterytour@gmail.com>
2023-06-05 20:19:08 +0300

    Selenide 1 homework after fixing

--- src/test/java/selenidetest/Selenide1.java
+++ src/test/java/selenidetest/Selenide1.java
@@ -37,21 +37,6 @@ public class Selenide1 {
 
         // cоздаем многострочный литерал
 
-        String JUnit5Example = """
-                @ExtendWith({SoftAssertsExtension.class})
-                        class Tests {
-                            @Test
-                            void test() {
-                                Configuration.assertionMode = SOFT;
-                                open("page.html");
 
-                                $("#first").should(visible).click();
-                                $("#second").should(visible).click();
-                            }
-                        }
-                        """;
-//проверка: кусок кода о JUnit 5 содержит текст из литерала
-
-        $("#user-content-3-using-junit5-extend-test-class").parent().sibling(0).shouldHave(text(JUnit5Example));
-    }
-}
+        $("#user-content-3-using-junit5-extend-test-class").parent().sibling(0).shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" + "class Tests {\n" + "  @Test\n" + "  void test() {\n" + "    Configuration.assertionMode = SOFT;\n" + "    open(\"page.html\");\n" + "\n" + "    $(\"#first\").should(visible).click();\n" + "    $(\"#second\").should(visible).click();\n" + "  }\n" + "}"));
+    }}
